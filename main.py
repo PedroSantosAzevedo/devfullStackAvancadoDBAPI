036f96cbf0b64f075f4ff4f722c6cfb8e21b9b98
Pedro Santos Azevedo <pedro8azevedo@gmail.com>
2025-08-31 17:00:41 -0300

    adiciona rotas

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import random
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 import httpx
+from models.location import *
+from models.locationArea import *    
 
 app = FastAPI()
 
@@ -13,8 +16,29 @@ def ping():
     return JSONResponse(content={"response": "pong"})
 
 
-@app.get("/abilities")
-async def get_abilities():
+#@app.get("/randomLocation")
+async def get_random_location():
+    location = random.randint(1,10)
     async with httpx.AsyncClient() as client:
-        response = await client.get("https://pokeapi.co/api/v2/ability/1")
-        return JSONResponse(content=response.json())
+        response = await client.get(f"https://pokeapi.co/api/v2/location/{location}")
+        if response.status_code == 200:
+            data = response.json()
+            loc =  Location(**data)
+            return JSONResponse(content=loc.model_dump_json())
+        else:
+            return JSONResponse(content={"error": "Location not found"}, status_code=404)
+
+@app.get("/randomArea/{area_id}")
+async def get_random_area(area_id: int):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"https://pokeapi.co/api/v2/location-area/{area_id}")
+        if response.status_code == 200:
+            data = response.json()
+            loc =  LocationArea(**data)
+            return JSONResponse(content=loc.model_dump_json())
+        else:
+            return JSONResponse(content={"error": "Location not found"}, status_code=404)
+
+@app.get("/randomLocationTest")
+async def get_random_location_test():
+    return await get_random_location()
